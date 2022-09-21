5db12d1fe6b270374008dae703b27c73d0b1fbc5
TheBigH50 <HFangmeier@outlook.com>
2022-09-21 11:16:31 -0500

    prettier

--- server.js
+++ server.js
@@ -5,30 +5,27 @@ import path from "path";
 
 const app = express();
 
-
 // Absolute file paths
 const homeFile = path.join(__dirname, "./public/index.html");
 const aboutFile = path.join(__dirname, "./public/about.html");
 
-
 // Create a GET request for the home and about routes
 app.get("/", (request, response) => {
-  try{
-  // sendFile takes in an ABSOLUTE filepath
-  response.sendFile(homeFile);
+  try {
+    // sendFile takes in an ABSOLUTE filepath
+    response.sendFile(homeFile);
   } catch (error) {
     console.error(error);
-    response.json({ success: false, msg: error.message});
+    response.json({ success: false, msg: error.message });
   }
 });
 
 app.get("/about", (request, response) => {
   try {
-    
-  response.sendFile(aboutFile);
+    response.sendFile(aboutFile);
   } catch (error) {
     console.error(error);
-    response.json({ success: false, msg: error.message});
+    response.json({ success: false, msg: error.message });
   }
 });
 
@@ -46,5 +43,5 @@ app.get("/about", (request, response) => {
 });*/
 
 app.listen(5000, () => {
-  console.log(`Server listening at http://localhost/5000`)
-})
+  console.log(`Server listening at http://localhost/5000`);
+});
