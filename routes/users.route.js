3c8122a39f8d63133ca4d820c6e1a7199ac7f85b
TheBigH50 <HFangmeier@outlook.com>
2022-09-21 15:02:24 -0500

    finish get route

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,38 +1,46 @@
 import express from "express";
 import db from "./mockdb";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/:id?", async(req, res, next) => {
-    try {
-        // TODO
-     } catch (error) {
-        next(error);
-     }
+router.get("/:id?", async (req, res, next) => {
+  try {
+    let { id } = req.params;
+    let data;
+    if (id == true) {
+      data = await db.getOne(id);
+      res.json(data);
+    } else {
+      data = await db.getAll();
+      res.json(data);
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post("/", async(req, res, next) => {
-    try {
-        // TODO
-     } catch (error) {
-        next(error);
-     }
+router.post("/", async (req, res, next) => {
+  try {
+    // TODO
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.put("/:id", async(req, res, next) => {
-    try {
-        // TODO
-     } catch (error) {
-        next(error);
-     }
+router.put("/:id", async (req, res, next) => {
+  try {
+    // TODO
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete("/:id", async(req, res, next) => {
-    try {
-        // TODO
-     } catch (error) {
-        next(error);
-     }
+router.delete("/:id", async (req, res, next) => {
+  try {
+    // TODO
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
