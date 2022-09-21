5ef14755220172e18722c1ed784b5a6d90d851e3
TheBigH50 <HFangmeier@outlook.com>
2022-09-21 14:07:46 -0500

    move routes into appropriate files, oops

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -2,35 +2,20 @@ import express from "express";
 
 const router = express.Routes();
 
-// Absolute file paths
-const homeFile = path.join(__dirname, "./public/index.html");
-const aboutFile = path.join(__dirname, "./public/about.html");
-
-// Routes are checked in order written (home, about, hobbits)
-// Create a GET request for the home and about routes
-router.get("/", (request, response, next) => {
-  try {
-    // sendFile takes in an ABSOLUTE filepath
-    response.sendFile(homeFile);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get("/about", (request, response, next) => {
-  try {
-    response.sendFile(aboutFile);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get("/hobbit", (request, response, next) => {
-  try {
-    response.sendFile(path.join(__dirname, "./public/hobbit.json"));
-  } catch (error) {
-    next(error);
-  }
-});
+// Route URL with params
+// Route with optional params = ?
+router.post("/hobbit/:name?", (request, response, next) => {
+    try {
+      // access the request body
+      let newHobbit = request.body;
+      // Access the request url parameters
+      let urlParams = request.params;
+      // Access the request query parameters
+      let queryParams = request.query
+      response.json({ success: true, hobbit: newHobbit, urlParams, queryParams });
+    } catch (error) {
+      next(error);
+    }
+  });
 
 export default router;
--- routes/page.routes.js
+++ routes/page.routes.js
@@ -2,17 +2,32 @@ import express from "express";
 
 const router = express.Router();
 
-// Route URL with params
-// Route with optional params = ?
-router.post("/hobbit/:name?", (request, response, next) => {
+// Absolute file paths
+const homeFile = path.join(__dirname, "./public/index.html");
+const aboutFile = path.join(__dirname, "./public/about.html");
+
+// Routes are checked in order written (home, about, hobbits)
+// Create a GET request for the home and about routes
+router.get("/", (request, response, next) => {
   try {
-    // access the request body
-    let newHobbit = request.body;
-    // Access the request url parameters
-    let urlParams = request.params;
-    // Access the request query parameters
-    let queryParams = request.query
-    response.json({ success: true, hobbit: newHobbit, urlParams, queryParams });
+    // sendFile takes in an ABSOLUTE filepath
+    response.sendFile(homeFile);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/about", (request, response, next) => {
+  try {
+    response.sendFile(aboutFile);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/hobbit", (request, response, next) => {
+  try {
+    response.sendFile(path.join(__dirname, "./public/hobbit.json"));
   } catch (error) {
     next(error);
   }
