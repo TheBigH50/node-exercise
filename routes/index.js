3f2edac89999f80aeb075b5823e1595dbf35e5a0
TheBigH50 <HFangmeier@outlook.com>
2022-09-22 13:59:34 -0500

    route through index

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRouter from "./users.route"
+import employeeRouter from "./employees.route"
 // TODO: import router from users.route
 
 const router = express.Router();
@@ -7,6 +8,8 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.send("working");
 });
+
+router.use("/employees", employeeRouter);
 
 router.use("/users", userRouter);
 // TODO: use the imported router to handle all routes matching "/users"
